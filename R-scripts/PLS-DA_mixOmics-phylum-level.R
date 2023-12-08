1ded5dc952e278b0a65c3a1df152ff457a315995
solomonmarc <152891969+solomonmarc@users.noreply.github.com>
2023-12-08 15:18:31 +0800

    Update PLS-DA_mixOmics-phylum-level.R

--- R-scripts/PLS-DA_mixOmics-phylum-level.R
+++ R-scripts/PLS-DA_mixOmics-phylum-level.R
@@ -1,7 +1,13 @@
 data <- read.csv("plsda-bacteria_sums.csv")
 category_mapping <- c("Base_of_tongue" = 1, "Floor_of_mouth" = 2, "Gum" = 3, "Larynx" = 4)
 data$Y_numeric <- category_mapping[data$Primary_Site]
-numeric_cols <- 2:77
+numeric_cols <- c("Firmicutes", "Actinobacteria", "Bacteroidetes", "Chlamydiae", "Proteobacteria",
+                  "k_Bacteria", "Cyanobacteria", "Chloroflexi", "Gemmatimonadetes", "Aquificae",
+                  "Acidobacteria", "Thermotogae", "Deinococcus", "Synergistetes", "Planctomycetes",
+                  "Fibrobacteres", "Tenericutes", "Spirochaetes", "Verrucomicrobia", "Nitrospirae",
+                  "Fusobacteria", "Thermodesulfobacteria", "Armatimonadetes", "Ignavibacteriae",
+                  "Chlorobi", "Caldiserica", "Lentisphaerae", "Candidatus_Cloacimonetes", "Chrysiogenetes",
+                  "Deferribacteres", "Elusimicrobia", "Rhodothermaeota", "Nitrospinae", "Dictyoglomi")
 
 library(mixOmics)
 plsda_result <- plsda(X = data[, numeric_cols], Y = data$Y_numeric, ncomp = 2)
@@ -11,6 +17,6 @@ plotIndiv(
   legend = TRUE, 
   comp = c(1, 2), 
   ellipse = TRUE, 
-  title = 'Phylum Level',
-  col.ind = df$Primary_site  # Use 'Primary_site' for coloring and legend
+  title = 'Bacteria: Phylum Level',
+  col.ind = df$Primary_site  
 )
