f957e689d13fc7ce437184fc20c3dd31be1a8560
solomonmarc <152891969+solomonmarc@users.noreply.github.com>
2023-12-28 16:02:09 +0800

    with edits on the legend

--- R-scripts/PLS-DA_mixOmics-phylum-level.R
+++ R-scripts/PLS-DA_mixOmics-phylum-level.R
@@ -1,5 +1,5 @@
 data <- read.csv("plsda-bacteria_sums.csv")
-category_mapping <- c("Base_of_tongue" = 1, "Floor_of_mouth" = 2, "Gum" = 3, "Larynx" = 4)
+category_mapping <- c("Gum" = 1, "Larynx" = 2, "Floor_of_mouth" = 3, "Base_of_tongue" = 4)
 data$Y_numeric <- category_mapping[data$Primary_Site]
 numeric_cols <- c("Firmicutes", "Actinobacteria", "Bacteroidetes", "Chlamydiae", "Proteobacteria",
                   "k_Bacteria", "Cyanobacteria", "Chloroflexi", "Gemmatimonadetes", "Aquificae",
@@ -13,10 +13,12 @@ library(mixOmics)
 plsda_result <- plsda(X = data[, numeric_cols], Y = data$Y_numeric, ncomp = 2)
 plotIndiv(
   plsda_result, 
+  asp = 1,
   ind.names = FALSE, 
   legend = TRUE, 
   comp = c(1, 2), 
   ellipse = TRUE, 
   title = 'Bacteria: Phylum Level',
+  pch = 16,
   col.ind = df$Primary_site  
 )
--- R-scripts/stacked-bar-plot-script.R
+++ R-scripts/stacked-bar-plot-script.R
@@ -1,13 +1,19 @@
 library(ggplot2)
 library(dplyr)
+library(RColorBrewer)
 
-data <- read.csv("data.csv")
+data <- read.csv("viruses_allsites_top5family.csv")
 data$Percentage <- as.numeric(data$Percentage)
+
+legend_order <- c("Papillomaviridae", "Polydnaviridae", "Herpesviridae", "Alloherpesviridae", "Betaflexiviridae", "Phycodnaviridae", "Partitiviridae", "Others")
 
 ggplot(data, aes(x = Site, y = Percentage, fill = Phylum)) +
   geom_bar(position = "fill", stat = "identity") +
-  labs(title = "100% Stacked Bar Chart",
+  labs(title = " ",
        x = "Site",
        y = "Percentage") +
-  scale_fill_brewer(palette = "Set3") + 
-  theme_minimal()
+  scale_fill_brewer(palette = "Dark2") + 
+  theme_minimal() +
+  guides(fill = guide_legend(title = "Phylum", key_glyph = "rect")) +
+  scale_fill_manual(values = brewer.pal(n = length(unique(data$Phylum)), name = "Dark2"),
+                    breaks = legend_order)
