813ff0b0108d5ac23929ec936664f53ab7e8ecbd
solomonmarc <152891969+solomonmarc@users.noreply.github.com>
2024-01-11 18:58:27 +0800

    Update stacked-bar-plot-script.R

--- R-scripts/stacked-bar-plot-script.R
+++ R-scripts/stacked-bar-plot-script.R
@@ -2,18 +2,19 @@ library(ggplot2)
 library(dplyr)
 library(RColorBrewer)
 
-data <- read.csv("viruses_allsites_top5family.csv")
+data <- read.csv("archaea_allsites_top4phyla.csv")
 data$Percentage <- as.numeric(data$Percentage)
 
-legend_order <- c("Papillomaviridae", "Polydnaviridae", "Herpesviridae", "Alloherpesviridae", "Betaflexiviridae", "Phycodnaviridae", "Partitiviridae", "Others")
+legend_order <- c("Crenarchaeota", "Euryarchaeota", "Thaumarchaeota", "Candidatus_Korarchaeota")
+data$Phylum <- factor(data$Phylum, levels = legend_order)
 
 ggplot(data, aes(x = Site, y = Percentage, fill = Phylum)) +
   geom_bar(position = "fill", stat = "identity") +
   labs(title = " ",
        x = "Site",
        y = "Percentage") +
-  scale_fill_brewer(palette = "Dark2") + 
+  scale_fill_brewer(palette = "Set2") + 
   theme_minimal() +
   guides(fill = guide_legend(title = "Phylum", key_glyph = "rect")) +
-  scale_fill_manual(values = brewer.pal(n = length(unique(data$Phylum)), name = "Dark2"),
+  scale_fill_manual(values = brewer.pal(n = length(unique(data$Phylum)), name = "Set2"),
                     breaks = legend_order)
